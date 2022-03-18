c51f4e1d9620e0dfecf2d84e443122870e55f690
silvydwiayup <silvydwiap88@gmail.com>
2022-03-18 15:53:01 +0800

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ src/tpmod4_1302202053_silv/dwi/HaloGeneric.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpmod4_1302202053_silv.dwi;
+
+/**
+ *
+ * @author dedyh
+ */
+public class HaloGeneric {
+    
+    public static void SapaUser(String name) {
+        System.out.printf("Halo user %s",name);
+    }
+
+    public static void main(String[] args) {
+        SapaUser("Silvy");
+    }
+}
+
